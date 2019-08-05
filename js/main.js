612dbed34aa9e6a17305a18b89524cd629617c5f
Jack Ground <Jaground@gmail.com>
2019-08-05 13:13:39 -0500

    initail commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+let initCount = 0;
+
+let decrementBtn = document.getElementById('decrement');
+let incrementBtn = document.getElementById('increment');
+let counter = document.getElementById('counter');
+let input = document.querySelector('input');
+
+
+incrementBtn.addEventListener('click', function() {
+    if(isNaN(parseInt(input.value))) return
+    initCount += parseInt(input.value)
+    render()
+});
+
+decrementBtn.addEventListener('click', function() {
+    if(isNaN(parseInt(input.value))) return
+    initCount -= parseInt(input.value)
+    render()    
+});
+
+
+render()
+function render() {
+    if(initCount < 0) counter.style.color = "red"
+    else counter.style.color = "black"
+    counter.textContent = initCount;
+}
+
